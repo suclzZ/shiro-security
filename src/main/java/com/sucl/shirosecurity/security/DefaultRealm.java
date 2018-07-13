033ecb6e970410ee715ff9a471b25f9cc90f6df2
suspring <sky_tk@126.com>
2018-07-13 09:06:50 +0800

    修改DefaultRealm

--- src/main/java/com/sucl/shirosecurity/security/DefaultRealm.java
+++ src/main/java/com/sucl/shirosecurity/security/DefaultRealm.java
@@ -4,10 +4,18 @@ import com.sucl.shirosecurity.entity.Account;
 import com.sucl.shirosecurity.service.AccountService;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 登录名登录
+ */
 public class DefaultRealm extends AuthorizingRealm {
 
     @Autowired
@@ -20,8 +28,16 @@ public class DefaultRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        Object principal = principalCollection.getPrimaryPrincipal();
-        return null;
+        //必须根据登录名等唯一字段增加权限，不然多realm都会执行此方法，若有一个有权限，另一个默认也会加上
+        AuthorizationInfo authorizationInfo = super.getAuthorizationInfo(principalCollection);
+
+        org.apache.shiro.authc.Account principal = (org.apache.shiro.authc.Account) principalCollection.getPrimaryPrincipal();
+        SimpleAuthorizationInfo simpleAuthorInfo = new SimpleAuthorizationInfo();
+        List<String> roles = new ArrayList<String>();
+        roles.add("admin");
+        simpleAuthorInfo.addRoles(roles);
+    //    simpleAuthorInfo.addStringPermissions(permissionList);
+        return simpleAuthorInfo;
     }
 
     /**
@@ -35,6 +51,7 @@ public class DefaultRealm extends AuthorizingRealm {
         UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
         String username = usernamePasswordToken.getUsername();
         Account account = accountService.getAcountByLoginname(username);
+        if(account == null) return null;
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(account,account.getPassword(),account.getUsername());
         return authenticationInfo;
     }
